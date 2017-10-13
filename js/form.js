3b098eb911a3b857327c8db963824fbc58afc6f4
Dmitriy Strungar <dmitriy.st92@mail.ru>
2017-10-13 22:25:58 +0500

    Добавил фильтры к изображению

--- js/form.js
+++ js/form.js
@@ -4,6 +4,7 @@ var uploadImage = document.querySelector('.upload-image');
 var uploadFormCancel = document.querySelector('.upload-form-cancel');
 var filter = document.querySelector('.upload-effect-controls');
 var imageFilter = document.querySelector('.effect-image-preview');
+var uploadForm = document.querySelector('.upload-form');
 
 var showImageLoader = function () {
   uploadOverplay.classList.add('hidden');
@@ -22,16 +23,28 @@ var deleteClass = function () {
   }
 };
 
+var addElementClass = function (node) {
+  var elementSelect = imageFilter.classList;
+  for (var i = 0; i <= elementSelect.length; i++) {
+    if(i === 1) {
+      elementSelect.add('effect-'+node);
+    } else {
+      elementSelect.remove(elementSelect[2]);
+      elementSelect.add('effect-'+node);
+    }
+  }
+};
+
 var addFilterImage = function (e) {
   // кроссбраузерная запись события
   var target = e && e.target || event.srcElement;
-  if (target.className != 'upload-effect-preview') {
-    var idElement = target.id;
+  if(target.checked) {
+    addElementClass(target.value);
   }
 };
 
 downloadFile.addEventListener('change', showCroppForm);
 uploadFormCancel.addEventListener('click', showImageLoader);
-filter.addEventListener('click', function (e) {
+uploadForm.addEventListener('click', function (e) {
   addFilterImage(event);
 });
